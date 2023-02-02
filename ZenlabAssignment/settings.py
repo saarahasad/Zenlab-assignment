0d88e76ddfaad202b7b4a304ef950c71ab160bdb
Saarah Asad <saarahasad26@gmail.com>
2023-02-02 18:53:23 +0530

    Update settings.py

--- ZenlabAssignment/settings.py
+++ ZenlabAssignment/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-+n&82b-g_fz^+10w^z(_7_98ls$lb9+ua7h81djdxhmjuc)qr2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
